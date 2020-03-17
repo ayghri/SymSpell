612105a43002054b2c75d88f8f512e7223b49d38
Éloi Rivard <azmeuk@gmail.com>
2020-03-17 14:07:05 +0100

    Used 'black' to format the code

--- symspell_python.py
+++ symspell_python.py
@@ -1,79 +1,101 @@
 import sys
 import utils
-            
+
+
 class SymSpell(object):
-    
-    def __init__(self,list_words,max_distance = 3):
-        self.max_distance = max_distance 
+    def __init__(self, list_words, max_distance=3):
+        self.max_distance = max_distance
         self.dictionary = {}
         self.max_length = 0
         self.progbar = utils.Progbar(len(list_words))
         self.build(list_words)
 
-    def build(self,words):
+    def build(self, words):
         n = len(words)
         self.progbar.__init__(n)
-        sys.stdout.write("Processing %d words to create SymSpell for edit distance %d \n"%(n,self.max_distance))
+        sys.stdout.write(
+            "Processing %d words to create SymSpell for edit distance %d \n"
+            % (n, self.max_distance)
+        )
         for i in range(n):
             self.progbar.add(1)
-            w=words[i]
+            w = words[i]
             if w in self.dictionary:
                 self.dictionary[w] = (self.dictionary[w][0], self.dictionary[w][1] + 1)
             else:
-                self.dictionary[w] = ([], 1)  
+                self.dictionary[w] = ([], 1)
                 self.max_length = max(self.max_length, len(w))
-                
-            if self.dictionary[w][1]==1:
-                deletes = utils.generate_deletes(w,self.max_distance)
+
+            if self.dictionary[w][1] == 1:
+                deletes = utils.generate_deletes(w, self.max_distance)
                 for d in deletes:
                     if d in self.dictionary:
                         self.dictionary[d][0].append(w)
                     else:
-                        self.dictionary[d] = ([w], 0)  
-                        
-    def correct(self,string):
-        if (len(string) - self.max_length) > self.max_distance:
-            return []        
-        corrections_dict = {}
-        min_correct_len = float('inf')        
-        queue = sorted(list(set([string]+utils.generate_deletes(string,self.max_distance))),key=len,reverse=True)
+                        self.dictionary[d] = ([w], 0)
 
-        while len(queue)>0:
+    def correct(self, string):
+        if (len(string) - self.max_length) > self.max_distance:
+            return []
+
+        corrections_dict = {}
+        min_correct_len = float("inf")
+        queue = sorted(
+            list(set([string] + utils.generate_deletes(string, self.max_distance))),
+            key=len,
+            reverse=True,
+        )
+
+        while len(queue) > 0:
             q_item = queue.pop(0)
-            
-            if ((len(corrections_dict)>0) and ((len(string)-len(q_item))>min_correct_len)):
+
+            if (len(corrections_dict) > 0) and (
+                (len(string) - len(q_item)) > min_correct_len
+            ):
                 break
+
             if (q_item in self.dictionary) and (q_item not in corrections_dict):
-                if (self.dictionary[q_item][1]>0):
-                    corrections_dict[q_item] = (self.dictionary[q_item][1],len(string) - len(q_item))
-                    if len(string)==len(q_item):
+                if self.dictionary[q_item][1] > 0:
+                    corrections_dict[q_item] = (
+                        self.dictionary[q_item][1],
+                        len(string) - len(q_item),
+                    )
+                    if len(string) == len(q_item):
                         break
+
                     elif (len(string) - len(q_item)) < min_correct_len:
                         min_correct_len = len(string) - len(q_item)
-                
+
                 for sc_item in self.dictionary[q_item][0]:
-                    if (sc_item not in corrections_dict):
-                        if len(q_item)==len(string):
+                    if sc_item not in corrections_dict:
+                        if len(q_item) == len(string):
                             item_dist = len(sc_item) - len(q_item)
-                            
+
                         item_dist = utils.levenshtein(sc_item, string)
-                        
-                        if item_dist>min_correct_len:
+
+                        if item_dist > min_correct_len:
                             pass
-                        elif item_dist<=self.max_distance:
-                            corrections_dict[sc_item] = (self.dictionary[sc_item][1], item_dist)
+
+                        elif item_dist <= self.max_distance:
+                            corrections_dict[sc_item] = (
+                                self.dictionary[sc_item][1],
+                                item_dist,
+                            )
                             if item_dist < min_correct_len:
                                 min_correct_len = item_dist
-                        
-                        corrections_dict = {k:v for k, v in corrections_dict.items() if v[1]<=min_correct_len}
-        
+
+                        corrections_dict = {
+                            k: v
+                            for k, v in corrections_dict.items()
+                            if v[1] <= min_correct_len
+                        }
+
         return corrections_dict
-    
-    def best(self,string):
+
+    def best(self, string):
         try:
             as_list = self.correct(string).items()
-            outlist = sorted(as_list, key=lambda item : (item[1][1], -item[1][0]))
+            outlist = sorted(as_list, key=lambda item: (item[1][1], -item[1][0]))
             return outlist[0][0]
         except:
             return None
-        
--- utils.py
+++ utils.py
@@ -1,35 +1,34 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
-
-
 import numpy as np
 import re
 import sys
 import time
 import collections
 
-def generate_deletes(string,max_distance):
+
+def generate_deletes(string, max_distance):
     deletes = []
     queue = [string]
     for d in range(max_distance):
         temp_queue = []
         for word in queue:
-            if len(word)>1:
+            if len(word) > 1:
                 for c in range(len(word)):  # character index
-                    word_minus_c = word[:c] + word[c+1:]
+                    word_minus_c = word[:c] + word[c + 1 :]
                     if word_minus_c not in deletes:
                         deletes.append(word_minus_c)
                     if word_minus_c not in temp_queue:
                         temp_queue.append(word_minus_c)
         queue = temp_queue
-        
+
     return deletes
 
+
 def levenshtein(string1, string2):
-    
-    if len(string1) < len(string2): return levenshtein(string2, string1)
-    if len(string2) == 0:return len(string1)
+
+    if len(string1) < len(string2):
+        return levenshtein(string2, string1)
+    if len(string2) == 0:
+        return len(string1)
 
     arr_string1 = np.array(list(string1))
     arr_string2 = np.array(list(string2))
@@ -38,63 +37,74 @@ def levenshtein(string1, string2):
     for s in arr_string1:
         current_row = last_row + 1
 
-        current_row[1:] = np.minimum(current_row[1:],
-                   np.add(last_row[:-1], arr_string2 != s))
+        current_row[1:] = np.minimum(
+            current_row[1:], np.add(last_row[:-1], arr_string2 != s)
+        )
 
         # Deletion (string2 grows shorter than string1):
-        current_row[1:] = np.minimum(current_row[1:],
-                current_row[0:-1] + 1)
+        current_row[1:] = np.minimum(current_row[1:], current_row[0:-1] + 1)
 
         last_row = current_row
 
     return last_row[-1]
 
+
 def read_corpus(path):
-    f = open(path,'r')
+    f = open(path, "r")
     words = []
     for w in f.readlines():
         words.append(w.split()[0].lower())
     f.close()
     return words
 
+
 def create_dictionary(fname):
-    dictionary = []    
+    dictionary = []
     with open(fname) as file:
         for line in file:
-            # separate by words by non-alphabetical characters      
-            words = re.findall('[a-z]+', line.lower())  
+            # separate by words by non-alphabetical characters
+            words = re.findall("[a-z]+", line.lower())
             for word in words:
                 dictionary.append(word)
     return dictionary
 
+
 def tweet_words(tweets):
     words = []
     for tweet in tweets:
-        for w in re.findall('[a-z]+', tweet):
+        for w in re.findall("[a-z]+", tweet):
             words.append(w)
     return words
 
 
-def correct_series(word,dict_corpus):
-    #print(word)
-    if word in dict_corpus: return word,True
-    if len(word)==0: return "",True
+def correct_series(word, dict_corpus):
+    # print(word)
+    if word in dict_corpus:
+        return word, True
+    if len(word) == 0:
+        return "", True
     i = 2
     corrections = {}
     order = 0
-    while(i<=len(word)):
-        #print(i)
+    while i <= len(word):
+        # print(i)
         if word[:i] in dict_corpus:
-            correct,possible = correct_series(word[i:],dict_corpus)
+            correct, possible = correct_series(word[i:], dict_corpus)
             if possible:
-                corrections[word[:i]+" "+correct]=(order,len(word[:i]+" "+correct))
+                corrections[word[:i] + " " + correct] = (
+                    order,
+                    len(word[:i] + " " + correct),
+                )
                 order += 1
         i = i + 1
-    
-    if len(corrections)==0:
+
+    if len(corrections) == 0:
         return "", False
     else:
-        return sorted(corrections.items(), key=lambda term, val: (val[1], val[0]))[0][0],True
+        return (
+            sorted(corrections.items(), key=lambda term, val: (val[1], val[0]))[0][0],
+            True,
+        )
 
 
 class Progbar(object):
@@ -111,8 +121,9 @@ class Progbar(object):
         interval: Minimum visual progress update interval (in seconds).
     """
 
-    def __init__(self, target, width=30, verbose=1, interval=0.05,
-                 stateful_metrics=None):
+    def __init__(
+        self, target, width=30, verbose=1, interval=0.05, stateful_metrics=None
+    ):
         self.target = target
         self.width = width
         self.verbose = verbose
@@ -122,9 +133,9 @@ class Progbar(object):
         else:
             self.stateful_metrics = set()
 
-        self._dynamic_display = ((hasattr(sys.stdout, 'isatty') and
-                                  sys.stdout.isatty()) or
-                                 'ipykernel' in sys.modules)
+        self._dynamic_display = (
+            hasattr(sys.stdout, "isatty") and sys.stdout.isatty()
+        ) or "ipykernel" in sys.modules
         self._total_width = 0
         self._seen_so_far = 0
         self._values = collections.OrderedDict()
@@ -146,45 +157,50 @@ class Progbar(object):
         for k, v in values:
             if k not in self.stateful_metrics:
                 if k not in self._values:
-                    self._values[k] = [v * (current - self._seen_so_far),
-                                       current - self._seen_so_far]
+                    self._values[k] = [
+                        v * (current - self._seen_so_far),
+                        current - self._seen_so_far,
+                    ]
                 else:
                     self._values[k][0] += v * (current - self._seen_so_far)
-                    self._values[k][1] += (current - self._seen_so_far)
+                    self._values[k][1] += current - self._seen_so_far
             else:
                 self._values[k] = v
         self._seen_so_far = current
 
         now = time.time()
-        info = ' - %.0fs' % (now - self._start)
+        info = " - %.0fs" % (now - self._start)
         if self.verbose == 1:
-            if (now - self._last_update < self.interval and
-                    self.target is not None and current < self.target):
+            if (
+                now - self._last_update < self.interval
+                and self.target is not None
+                and current < self.target
+            ):
                 return
 
             prev_total_width = self._total_width
             if self._dynamic_display:
-                sys.stdout.write('\b' * prev_total_width)
-                sys.stdout.write('\r')
+                sys.stdout.write("\b" * prev_total_width)
+                sys.stdout.write("\r")
             else:
-                sys.stdout.write('\n')
+                sys.stdout.write("\n")
 
             if self.target is not None:
                 numdigits = int(np.floor(np.log10(self.target))) + 1
-                barstr = '%%%dd/%d [' % (numdigits, self.target)
+                barstr = "%%%dd/%d [" % (numdigits, self.target)
                 bar = barstr % current
                 prog = float(current) / self.target
                 prog_width = int(self.width * prog)
                 if prog_width > 0:
-                    bar += ('=' * (prog_width - 1))
+                    bar += "=" * (prog_width - 1)
                     if current < self.target:
-                        bar += '>'
+                        bar += ">"
                     else:
-                        bar += '='
-                bar += ('.' * (self.width - prog_width))
-                bar += ']'
+                        bar += "="
+                bar += "." * (self.width - prog_width)
+                bar += "]"
             else:
-                bar = '%7d/Unknown' % current
+                bar = "%7d/Unknown" % current
 
             self._total_width = len(bar)
             sys.stdout.write(bar)
@@ -196,39 +212,42 @@ class Progbar(object):
             if self.target is not None and current < self.target:
                 eta = time_per_unit * (self.target - current)
                 if eta > 3600:
-                    eta_format = '%d:%02d:%02d' % (eta // 3600, (eta % 3600) // 60, eta % 60)
+                    eta_format = "%d:%02d:%02d" % (
+                        eta // 3600,
+                        (eta % 3600) // 60,
+                        eta % 60,
+                    )
                 elif eta > 60:
-                    eta_format = '%d:%02d' % (eta // 60, eta % 60)
+                    eta_format = "%d:%02d" % (eta // 60, eta % 60)
                 else:
-                    eta_format = '%ds' % eta
+                    eta_format = "%ds" % eta
 
-                info = ' - ETA: %s' % eta_format
+                info = " - ETA: %s" % eta_format
             else:
                 if time_per_unit >= 1:
-                    info += ' %.0fs/step' % time_per_unit
+                    info += " %.0fs/step" % time_per_unit
                 elif time_per_unit >= 1e-3:
-                    info += ' %.0fms/step' % (time_per_unit * 1e3)
+                    info += " %.0fms/step" % (time_per_unit * 1e3)
                 else:
-                    info += ' %.0fus/step' % (time_per_unit * 1e6)
+                    info += " %.0fus/step" % (time_per_unit * 1e6)
 
             for k in self._values:
-                info += ' - %s:' % k
+                info += " - %s:" % k
                 if isinstance(self._values[k], list):
-                    avg = np.mean(
-                        self._values[k][0] / max(1, self._values[k][1]))
+                    avg = np.mean(self._values[k][0] / max(1, self._values[k][1]))
                     if abs(avg) > 1e-3:
-                        info += ' %.4f' % avg
+                        info += " %.4f" % avg
                     else:
-                        info += ' %.4e' % avg
+                        info += " %.4e" % avg
                 else:
-                    info += ' %s' % self._values[k]
+                    info += " %s" % self._values[k]
 
             self._total_width += len(info)
             if prev_total_width > self._total_width:
-                info += (' ' * (prev_total_width - self._total_width))
+                info += " " * (prev_total_width - self._total_width)
 
             if self.target is not None and current >= self.target:
-                info += '\n'
+                info += "\n"
 
             sys.stdout.write(info)
             sys.stdout.flush()
@@ -236,14 +255,13 @@ class Progbar(object):
         elif self.verbose == 2:
             if self.target is None or current >= self.target:
                 for k in self._values:
-                    info += ' - %s:' % k
-                    avg = np.mean(
-                        self._values[k][0] / max(1, self._values[k][1]))
+                    info += " - %s:" % k
+                    avg = np.mean(self._values[k][0] / max(1, self._values[k][1]))
                     if avg > 1e-3:
-                        info += ' %.4f' % avg
+                        info += " %.4f" % avg
                     else:
-                        info += ' %.4e' % avg
-                info += '\n'
+                        info += " %.4e" % avg
+                info += "\n"
 
                 sys.stdout.write(info)
                 sys.stdout.flush()
